12521500eeb3f10ba4af5849a3c2778a4ba51183
zacharidis <zacharidis@icloud.com>
2022-05-17 14:07:21 +0300

    MiniEight Unfinished added

---
+++ src/MiniEight.java
@@ -0,0 +1,16 @@
+/**
+ * εξομοιωση παιχνιδιου τριλιζας 2 ατομων - παικτων
+ * @author zacharidis
+ *
+ */
+
+public class MiniEight {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
